d28662b075b97b339cc3579a36cffe2d14017076
Megha331 <148079324+Megha331@users.noreply.github.com>
2023-10-17 11:41:34 +0530

    Add files via upload

---
+++ sorting.js
@@ -0,0 +1,6 @@
+//Perform sorting of an array in descending order.
+var  numbers = [5, 2, 9, 1, 5, 6];
+ console.log(numbers.sort());//output in ascending order
+ console.log(numbers.reverse());//will be in descending order
+
+
